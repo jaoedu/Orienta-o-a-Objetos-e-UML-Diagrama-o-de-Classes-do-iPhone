30219dfef6350c5edc958189da73f982e315d609
João Mota <ejoao282@gmail.com>
2023-10-15 13:42:20 -0300

    As funcionalidades ja foram implementadas, agora so falta o UML

---
+++ src/Iphone.java
@@ -0,0 +1,34 @@
+
+    class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorWeb {
+
+        public void tocar() {
+            System.out.println("Tocar música");
+        }
+        public void pausar() {
+            System.out.println("pausar música");
+        }
+        public void selecionarMusica() {
+            System.out.println("Selecionar Música desejada");
+        }
+
+        public void ligar() {
+            System.out.println("ligando para Steve jobs");
+        }
+        public void atender() {
+            System.out.println("Atendendo Steve jobs");
+        }
+        public void CorreioDevoz() {
+            System.out.println("deixe sua mensagem após o  bip");
+        }
+
+
+        public void exibirPagina() {
+            System.out.println("Exibindo a página");
+        }
+        public void adicionarNovaAba() {
+            System.out.println("adicionando uma nova aba");
+        }
+        public void atualizarPagina() {
+            System.out.println("atualizando página");
+        }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        iphone.atender();
+        iphone.ligar();
+        iphone.CorreioDevoz();
+        iphone.selecionarMusica();
+        iphone.tocar();
+        iphone.pausar();
+        iphone.adicionarNovaAba();
+        iphone.exibirPagina();
+        iphone.atualizarPagina();
+    }
+}
